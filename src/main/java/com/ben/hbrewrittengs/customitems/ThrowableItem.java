5d815d6aaee61ee396cd74d5b86528409bf26101
UndauntedPhish <benrozario2012@gmail.com>
2020-05-07 03:00:00 -0400

    Made item throw trajectory start at the player's face-hand area, instead of the head-above head area for easier aiming.

--- src/main/java/com/ben/hbrewrittengs/customitems/ThrowableItem.java
+++ src/main/java/com/ben/hbrewrittengs/customitems/ThrowableItem.java
@@ -13,10 +13,10 @@ public class ThrowableItem
     // Throws the selected item
     public static Entity throwItem(Material item, Player thrower)
     {
-        Location throwOrigin = thrower.getLocation().add(0.0, 1.0, 0.0);
+        Location throwOrigin = thrower.getLocation().add(0.0, 0.5, 0.0);
 
         Entity thrownItem = thrower.getWorld().dropItemNaturally(throwOrigin, new ItemStack(item));
-        thrownItem.setVelocity(throwOrigin.getDirection().add(new Vector(0.0, 0.1, 0.0)).multiply(1.3));
+        thrownItem.setVelocity(throwOrigin.getDirection().add(new Vector(0.0, 0.15, 0.0)).multiply(1.3));
         thrower.playSound(throwOrigin, Sound.ENTITY_SNOWBALL_THROW, 1.0F, 0.5F);
 
         return thrownItem;
